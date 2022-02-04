c480c978a81a8cba2caf709368ca137f15a8e874
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2022-02-04 16:42:15 +0530

    Add sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :articles, except: [:destroy]
   delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
   get 'articles/:id/delete' => 'articles#destroy'
+  get 'signup' => 'users#new'
+  resources :users, except: [:new]
 end
